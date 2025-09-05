c8fe356e0c91794786a2cada03af000366943d9b
Eshaan Bhargav Patel <96751029+ebp012@users.noreply.github.com>
2025-09-05 08:33:09 -0400

    Translations

--- scripts/translations.js
+++ scripts/translations.js
@@ -19,6 +19,15 @@ var translations = {
     welcomeText: 'Aryabhata is your <i>free</i> one-stop shop to a world of wonderful math, science, and linguistics tools.<br/>Below, you can find a variety of apps which you&#x0027;ll definitely find useful.',
     scientificHeader: 'Math and Science Tools',
     linguisticHeader: 'Memory and Linguistic Tools',
+    // Graphing Tab
+    functionsHeader: 'Functions',
+    centreBtn: 'Centre',
+    roundTitle: 'Round',
+    scaleTitle: 'Scale',
+    panningBtn: 'Panning',
+    xivBtn: 'XI Viewport',
+    gridlinesBtn: 'Gridlines',
+    tickmarksBtn: 'Tickmarks',
   },
   fr: {
     // Navigation
@@ -37,6 +46,15 @@ var translations = {
     welcomeText: 'Aryabhata est votre appli <i>gratuit</i> pour un monde des outiles mathématique, scientifique, et linguistique.<br/>Dessous, vous pouvez touver beaucoup des mini-applis que vous penserez très utiles.',
     scientificHeader: 'Outiles Scientifiques et Mathematiques',
     linguisticHeader: 'Outiles Linguistiques et pour le Memoire',
+    // Graphing Tab
+    functionsHeader: 'Fonctions',
+    centreBtn: 'Centrer',
+    roundTitle: 'Arrondir',
+    scaleTitle: 'Échelonner',
+    panningBtn: 'Fenêtre Primaire',
+    xivBtn: 'Fenêtre XI',
+    gridlinesBtn: 'Quadrillage',
+    tickmarksBtn: 'Coches',
   },
   applyLang: function () {
     switch (lang) {
@@ -68,6 +86,17 @@ var translations = {
         qs('#launcher-welcome-text').innerHTML = translations.en.welcomeText;
         qs('#launcher-scientific').innerHTML = translations.en.scientificHeader;
         qs('#launcher-linguistic').innerHTML = translations.en.linguisticHeader;
+        // Graphing Tab
+        qs('#functions-header').innerHTML = translations.en.functionsHeader;
+        qs('#graph-title').innerHTML = translations.en.graphingTab;
+        qs('#function-title').innerHTML = translations.en.functionsHeader;
+        qs('#centre-btn').innerHTML = translations.en.centreBtn;
+        qs('#round-title').innerHTML = translations.en.roundTitle;
+        qs('#scale-title').innerHTML = translations.en.scaleTitle;
+        qs('#panning-btn').innerHTML = translations.en.panningBtn;
+        qs('#xiviewport-btn').innerHTML = translations.en.xivBtn;
+        qs('#gridlines-btn').innerHTML = translations.en.gridlinesBtn;
+        qs('#tickmarks-btn').innerHTML = translations.en.tickmarksBtn;
         break;
       case 'fr':
         qs('#scientificTab').style.fontSize = '10pt';
@@ -97,6 +126,17 @@ var translations = {
         qs('#launcher-welcome-text').innerHTML = translations.fr.welcomeText;
         qs('#launcher-scientific').innerHTML = translations.fr.scientificHeader;
         qs('#launcher-linguistic').innerHTML = translations.fr.linguisticHeader;
+        // Graphing Tab
+        qs('#functions-header').innerHTML = translations.fr.functionsHeader;
+        qs('#graph-title').innerHTML = translations.fr.graphingTab;
+        qs('#function-title').innerHTML = translations.fr.functionsHeader;
+        qs('#centre-btn').innerHTML = translations.fr.centreBtn;
+        qs('#round-title').innerHTML = translations.fr.roundTitle;
+        qs('#scale-title').innerHTML = translations.fr.scaleTitle;
+        qs('#panning-btn').innerHTML = translations.fr.panningBtn;
+        qs('#xiviewport-btn').innerHTML = translations.fr.xivBtn;
+        qs('#gridlines-btn').innerHTML = translations.fr.gridlinesBtn;
+        qs('#tickmarks-btn').innerHTML = translations.fr.tickmarksBtn;
         break;
     };
   },
