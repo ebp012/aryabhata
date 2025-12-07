49a7e19b17c93e323b95ec9c96e18de817818c7a
Eshaan Bhargav Patel <96751029+ebp012@users.noreply.github.com>
2025-12-07 08:39:14 -0500

    Nuke all SVG filters on slow devices

--- styles/sigma.js
+++ styles/sigma.js
@@ -3,5 +3,10 @@ requestAnimationFrame(() => {
   let t1 = performance.now();
   if (t1 - t0 > 100) {
     document.documentElement.classList.add("lowfx");
+
+    // break all filter references
+    document.querySelectorAll("filter[id]").forEach(f => {
+      f.removeAttribute("id");
+    });
   }
 });
